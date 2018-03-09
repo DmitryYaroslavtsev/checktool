704b2e900d00ec57462c1ca95ef25baa712017ea
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2018-03-09 13:53:35 +0300

    Add http request

--- src/main/src/main/java/checktool/RCApi.java
+++ src/main/src/main/java/checktool/RCApi.java
@@ -1,5 +1,14 @@
 package checktool;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RCApi {
 
     String hostname = null;
@@ -11,5 +20,19 @@ public class RCApi {
         this.application = application;
         this.userCredentials = user;
         this.hostname = hostname;
+    }
+
+    void openHttpConnection(String hostname) throws IOException {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet(hostname);
+            HttpResponse response = client.execute(request);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                System.out.println(str);
+            }
+        }
     }
 }
